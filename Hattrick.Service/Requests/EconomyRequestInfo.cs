5e6e21cf54179f4444442c619018007d84e72ce7
Stefan Kamphuis <skamphuis@users.noreply.github.com>
2010-05-13 16:55:33 +0000

    Added file=economy

---
+++ Hattrick.Service/Requests/EconomyRequestInfo.cs
@@ -0,0 +1,25 @@
+using Hattrick.Service.BaseClasses;
+
+namespace Hattrick.Service.Requests
+{
+    public class EconomyRequestInfo : BaseRequestInfo
+    {
+        public EconomyRequestInfo()
+        {
+            // Set defaults here
+        }
+
+        #region Enumerations
+
+        // Create enumeration here for properties
+        // that allow only a set of values
+
+        #endregion
+
+        #region Properties
+
+        // Create properties for parameters
+
+        #endregion
+    }
+}
